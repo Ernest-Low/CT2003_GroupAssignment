b8c2098c15593a32b9b494d25c72f9db941a99e3
Chua Zhisheng <chuazhisheng@hotmail.com>
2025-10-28 20:32:53 +0800

    update

--- src/Student.java
+++ src/Student.java
@@ -1,31 +1,62 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Student extends User{
+	private static final int MAX_APPLICATION = 3;
+	private String studentID; // remove if it's implemented in User class
 	private int year; 
-	private String major; /* year & major, for determining which internship can be applied/shown */
-	private Internship[] internship = new Internship[3]; /* if there's no internship class, can be just a list of internship id from csv */
-	private int[] internshipId = new int[3]; /* alternative */
-	
-	
+	private String major; // year & major, for determining which internship can be applied/shown
+	private Internship[] internships = new Internship[MAX_APPLICATION];
+	private int applicationCount;
+
+	public Student(String name, String email, String studentID, int year, String major) {
+		super(name, email); // assuming name and email are in user class
+		this.studentID = studentID;
+		this.major = major;
+		this.applicationCount = this.count();
+	}
+
 	/* register all students */
 	public static void register() {
-		/* auto register by reading CSV file */
+		// auto register by reading CSV file
 		return;
 	}
 	
-	public void exploreInternships() {
-		/* shows only internship that are eligible for student. Y1/2 - Basic, Y3 - all */
-		return;
+	public Internship[] exploreInternships(Internship[] listings) {
+		// shows only internship that are eligible for student. Y1/2 - Basic, Y3 - all
+		
+		if (this.year < 3){
+			// return internships
+		}
+		else {
+			// display all
+		}
 	}
 	
-	public void applyInternship() {
-		/* max 3 applications */
-		/* default status will be 'Pending' */
-		return;
+	public boolean applyInternship(Internship listing) {
+		// max 3 applications
+		// default status will be 'Pending'
+		if (this.applicationCount < 3) {
+			this.internship[applicationCount] = listing;
+
+			this.applicationCount++;
+			return true;
+		}
+		// If applcation fails
+		return false;
 	}
 	
 	public void viewApplications() {
-		/* application status, "Successful" > can be accepted */
-		/* remove other applications if 1 is accepted */
+		// application status, "Successful" > can be accepted */
+		// remove other applications if 1 is accepted */
 		return;
 	}
 	
+	public int count() {
+		// reads student-internship relation csv
+		try (Scanner sc = new Scanner(new File("./data/student_internship.csv"))) {
+			
+		}
+
+	}
 }
