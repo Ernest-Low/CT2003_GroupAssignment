8ed78faa5200280b96cf31b2bf10a3031ac28fab
ethanyap10 <ethanyap10@yahoo.com>
2025-11-07 22:19:44 +0800

    Created a few new CSVMethods file to breakdown things the methods we need In each file have added comments on what it will do

---
+++ CSVMethods/CSVBeutify.java
@@ -0,0 +1,8 @@
+package CSVMethods;
+
+
+//This class file is not to be used directly, its mainly for CSV file to be printed out in a nice format
+
+public class CSVBeutify {
+    
+}
---
+++ CSVMethods/CSVFilter.java
@@ -0,0 +1,8 @@
+package CSVMethods;
+
+//Ethan - There shouldnt be a need to use this file, you should use the read file instead.
+//This file is for only filtering data and does not print any data, Please use readfilter in CSVRead.java.
+
+public class CSVFilter {
+    
+}
