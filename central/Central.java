b1ec3f53a52920cb5f2855003dfbcb8be9a059f6
Ernest Low <ernestlow32@gmail.com>
2025-11-13 17:01:01 +0800

    edit central

--- central/Central.java
+++ central/Central.java
@@ -1,6 +1,6 @@
 package central;
 
-import enums.Major;
+// import enums.Major;
 
 import model.Student;
 import student.StudentMenu;
@@ -10,16 +10,16 @@ import StaffFiles.staffmainnew;
 import companyRep.CompanyRepMain;
 import config.Services;
 import dtos.LoginInfo;
-import student.*;
-import StaffFiles.*;
+// import student.*;
+// import StaffFiles.*;
 
-import dtos.*;
+// import dtos.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
 
-import CSVMethods.*;
+// import CSVMethods.*;
 
 import login.AuthController;
 // import login.UpdatePasswordController;
@@ -32,9 +32,9 @@ public class Central {
     private staffmainnew careerStaffMain;
 
     //CSV files
-    private static final String STAFF_CSV = "data/careerStaff_list.csv";
-    private static final String STUDENT_CSV = "data/students_list.csv";
-    private static final String CREP_CSV = "data/companyReps_list.csv";
+    // private static final String STAFF_CSV = "data/careerStaff_list.csv";
+    // private static final String STUDENT_CSV = "data/students_list.csv";
+    // private static final String CREP_CSV = "data/companyReps_list.csv";
 
     public Central(Services services) {
         this.services = services;
@@ -43,7 +43,7 @@ public class Central {
     public void centralController() {
         // * Entry point from main
 
-        // ! Login, get user ID & user type back
+        // * Login, get user ID & user type back
         AuthController authController = new AuthController();
         LoginInfo loginInfo = null;
         while (loginInfo == null) {
@@ -61,50 +61,42 @@ public class Central {
             case CAREERSTAFF -> careerStaffGateway(loginInfo);
             case COMPANYREP -> companyRepGateway(loginInfo);
             default -> System.out.println("Logic error");
-        // ! Temp, pretend got the logininfo back from login
-        // ! Change this logininfo's UserType to your menu
-
-        // TODO: Pull from CSV the user
-        // TODO: Maybe update CSV to pull by ID, maybe add GUID (mimic database fully)
-
-        // TODO: Pull from CSV the user based on usertype
-        // TODO: It will be calling the different services anyway, should I just put it there?
-        // TODO: Could rename it as gateway (idk why i feel like naming it that)
-
-        switch (loginInfo.getUserType()) {
-
-            case STUDENT:
-
-                List<String []> userData = new ArrayList<>();
-
-                userData = CSVRead.readByID(STUDENT_CSV, loginInfo.getID(), "ID");
-
-                String[] userRow = userData.get(0);
-                int year = Integer.parseInt(userRow[3]);
-                Major Major = enums.Major.valueOf(userRow[4].toUpperCase());
-                //System.out.println(Arrays.toString(userData));
-                Student Student = new Student(userRow[0], userRow[1], userRow[2], year, Major);
-            
-                studentMenu(Student, loginInfo);
-            case CAREERSTAFF:
-
-                List<String[]> staffData = new ArrayList<>();
-
-                userData = CSVRead.readByID(STAFF_CSV, loginInfo.getID(), "ID");
-                String[] staffRow = staffData.get(0);
-
-                CareerStaff CareerStaff = new CareerStaff(staffRow[0], staffRow[1], staffRow[2], staffRow[3]);
-            
-                careerStaffMenu(CareerStaff, loginInfo);
-                break;
-            case COMPANYREP:
-            
-                cRepGateway(loginInfo);
-                break;
-
-            default:
-                System.out.println("Logic error");
         }
+
+        // switch (loginInfo.getUserType()) {
+
+        //     case STUDENT:
+
+        //         List<String []> userData = new ArrayList<>();
+
+        //         userData = CSVRead.readByID(STUDENT_CSV, loginInfo.getID(), "ID");
+
+        //         String[] userRow = userData.get(0);
+        //         int year = Integer.parseInt(userRow[3]);
+        //         Major Major = enums.Major.valueOf(userRow[4].toUpperCase());
+        //         //System.out.println(Arrays.toString(userData));
+        //         Student Student = new Student(userRow[0], userRow[1], userRow[2], year, Major);
+            
+        //         studentMenu(Student, loginInfo);
+        //     case CAREERSTAFF:
+
+        //         List<String[]> staffData = new ArrayList<>();
+
+        //         userData = CSVRead.readByID(STAFF_CSV, loginInfo.getID(), "ID");
+        //         String[] staffRow = staffData.get(0);
+
+        //         CareerStaff CareerStaff = new CareerStaff(staffRow[0], staffRow[1], staffRow[2], staffRow[3]);
+            
+        //         careerStaffMenu(CareerStaff, loginInfo);
+        //         break;
+        //     case COMPANYREP:
+            
+        //         cRepGateway(loginInfo);
+        //         break;
+
+        //     default:
+        //         System.out.println("Logic error");
+        // }
     }
 
     private void studentGateway(LoginInfo loginInfo) {
@@ -112,8 +104,6 @@ public class Central {
 
         Student student = services.studentService.getStudentByID(loginInfo.getID());
 
-        // ! Mock user
-        student = new Student("S000001T", "U1000001A", "Aaron Tan", 1, Major.COMPUTER_SCIENCE);
 
         this.studentMain = new StudentMenu(student);
         this.studentMain.StudentController(loginInfo);
@@ -123,9 +113,6 @@ public class Central {
         // * Entry point Career Staff
 
         CareerStaff careerStaff = services.careerStaffService.getCareerStaffByID(loginInfo.getID());
-
-        // ! Mock user
-        careerStaff = new CareerStaff("C000001S", "jtan001", "John Tan", "Career Advisory");
 
         this.careerStaffMain = new staffmainnew(careerStaff);
         this.careerStaffMain.staffEntry();
